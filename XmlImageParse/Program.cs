7699c68e35d011470bae1de628dd91d8ee2ac493
rekk88 <maicol.matteoli@gmail.com>
2022-12-13 22:36:09 +0100

    add: Helper class start debug reading xml node attributes

--- XmlImageParse/Program.cs
+++ XmlImageParse/Program.cs
@@ -3,26 +3,35 @@ using System.Xml.Linq;
 using System.Xml;
 using System;
 using System.IO;
+using XmlImageParse.XMLParse.Helpers;
+
 public class Program
 {
 
     internal static void Main(string[] args)
     {
-        string dir = Directory.GetCurrentDirectory() + @"\XMLParse\XMLSources\myConfig.xml";
-        XElement xmlFromFile = XElement.Load(@"B:\Repos\rekk88\XmlImageParse\XmlImageParse\XMLParse\XMLSources\myConfig.xml");
-
-        //var test = from glz in xmlFromFile.Element("myConfig")
-        //           where glz.Elements("glz")
-        //           select glz;
-        //Console.WriteLine(XElement.Parse((string)xmlFromFile));
+        Console.WriteLine(Helpers.getProjectDirectory());
+        string xmlPath = Helpers.getProjectDirectory() + @"XMLParse\XMLSources\myConfig.xml";
+        XElement xmlFromFile = XElement.Load(xmlPath);
+        XmlDocument doc = new XmlDocument(); //creates a new XmlDocument obj
+        doc.Load(xmlPath); //loads the xml file content in the XmlDocument doc
+        Console.WriteLine(doc);
+        //while doc.hasChildNodes 
+        //append the node name to the child child node
+        //i want this part of code to be recursive
+        XmlNodeList nodes = doc.ChildNodes;
         XmlReader reader = xmlFromFile.CreateReader();
         while (reader.Read())
         {
             //Console.WriteLine(reader);
+            Console.WriteLine(reader.LocalName);
             Console.WriteLine(reader);
+            
+
             //Console.WriteLine(reader.Value);
             //Console.WriteLine(reader.GetAttribute(reader.Name));
             //Console.WriteLine(reader.NameTable);
+            //reader.MoveToNextAttribute();
         }
         // Console.WriteLine(xmlFromFile); //check file
         // XmlTextReader reader = new XmlTextReader(dir);
---
+++ XmlImageParse/XMLParse/Helpers/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlImageParse.XMLParse.Helpers
+{
+    public static class Helpers
+    {
+        public static string getProjectDirectory()
+        {
+            string dir = AppDomain.CurrentDomain.BaseDirectory;
+            if (dir.Contains("bin"))
+            {
+                dir = dir.Substring(0, dir.IndexOf("bin"));
+            }
+            Console.WriteLine(dir);
+            return dir;
+        }
+    }
+}
